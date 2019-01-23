0109d1d2ddd0568c8f1d1b761b2f523eb7b7c051
Eilin Castillo <castilloeilin@gmail.com>
2019-01-23 13:19:15 -0300

    changes in yoga class message

--- static/js/modalRegularClass.js
+++ static/js/modalRegularClass.js
@@ -21,8 +21,22 @@ window.addEventListener('click', outsideClick);
 // Open
 function openModalYoga() {
   modal.style.display = 'block';
-  $("#description").text('Descripción del curso de yoga');
-  $("#profesor").text('Profesor: Pedro Pérez');
+  var obj = $("#description").text('A traves de la practica del Yoga se logra  equilibrio  ' +
+      'y armonia del cuerpo, mente y espíritu. Los beneficios del Yoga son innumerables a nivel fisico, mental y espiritual. \n \n' +
+      'Beneficios:\n' +
+      '\n' +
+      'Físicos: ayuda a prevenir enfermedades, fortalece y flexibiliza todo el  cuerpo, estimula el funcionamiento de las glándulas tiroides y paratiroides, ' +
+      'desarrollo de la respiración consciente-mayor oxigenación, refuerza el sistema inmunológico y endocrino, proporciona más equilibrio físico, fortalece músculos y cuerpo, ' +
+      'ayuda al sistema neuromuscular y las articulaciones, genera estados de relajación que reducen el ritmo cardiaco y la presión sanguínea, proporciona flexibilidad a  la columna vertebral.\n' +
+      '\n' +
+      'Mentales: disminuye la ansiedad, la depresión, el estrés, ayuda a potenciar la capacidad de concentración y la memoria, aumenta la creatividad, fortalece la autoestima y ' +
+      'eleva el nivel de conciencia del individuo de si mismo como del todo del cual forma parte. \n' +
+      '\n' +
+      'Espirituales: en estos beneficios juega parte muy importante la meditación, la cual representa la esencia espiritual de la práctica del yoga y que nos conduce a la ' +
+      'unión perfecta del cuerpo y la mente en uno solo, y nos permite encontrarnos con nuestro propio ser y yo interior, donde verdaderamente se encuentra Dios. ' +
+      'El principal beneficio es el estado de conexión consigo mismo.');
+  obj.html(obj.html().replace(/\n/g,'<br/>'));
+  $("#profesor").text('Duracion de la actividad: 1 hora');
   $("#modalTitle").text('Yoga');
 }
 
@@ -35,8 +49,10 @@ function openModalChikun() {
 
 function openModalMeditation() {
   modal.style.display = 'block';
-  $("#description").text('Descripción del curso de meditación');
-  $("#profesor").text('Profesor: Pedro Pérez');
+  var obj = $("#description").text('La sesión diaria de meditación va dirigida a proporcionar herramientas para que el participante pueda durante y después de la clase encontrar paz y armonía interior, mediante una práctica guiada enfocados en la postura correcta, la respiración, generar un estado de relajación, en la observación de los pensamientos y visualización.\n \n' +
+      'La meditación nos proporciona fortalecimiento de nuestro ser espiritual, desarrollo de la conciencia, fortalece la concentración, potencia las habilidades y capacidades, mayor estado de felicidad y plenitud, cambio en la forma de abordar las situaciones y problemas, mayor empatía y fuerza de voluntad.');
+  obj.html(obj.html().replace(/\n/g,'<br/>'));
+  $("#profesor").text('Duracion de la actividad: 1 hora');
   $("#modalTitle").text('meditación');
 }
 
